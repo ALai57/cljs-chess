ca96b2a0b8bfc2a7fdf781161c86158d93afb863
Andrew Lai <alai@splashfinancial.com>
2022-03-05 13:37:25 -0600

    Working board

--- src/cljs_chess/components/chess_square.cljs
+++ src/cljs_chess/components/chess_square.cljs
@@ -1,19 +1,23 @@
-(ns cljs-chess.components.chess-square)
+(ns cljs-chess.components.chess-square
+  (:require [taoensso.timbre :refer-macros [infof]]))
 
 (defn get-image
   [{:keys [piece owner]}]
   (str "images/" owner "-" piece ".svg"))
 
 (defn chess-square
-  [{:keys [dim id style piece]
+  [{:keys [dim id style piece on-click background-color]
     :or   {dim "100px"}}]
-  (println "PIECE" piece)
-  [:div.background-darkgrey.hover-icon {:style (merge {:width               dim
-                                                       :height              dim
-                                                       :float               "left"
-                                                       :background-size     "80% 80%"
-                                                       :background-position "center"
-                                                       :background-repeat   "no-repeat"
-                                                       :background-image    (str "url(" (get-image piece) ")")}
-                                                 style)
-                                        :id    id}])
+  (infof "PIECE %s ACTIVE? %s" piece active?)
+  [:div.background-darkgrey.hover-icon.chess-square
+   {:style    (merge {:width               dim
+                      :height              dim
+                      :float               "left"
+                      :background-size     "80% 80%"
+                      :background-position "center"
+                      :background-repeat   "no-repeat"
+                      :background-image    (str "url(" (get-image piece) ")")
+                      :background-color    background-color}
+                style)
+    :on-click on-click
+    :id       id}])
