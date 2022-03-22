a5b3059990f732dccc9dc1e4587f079f74b6f029
Andrew Lai <alai@splashfinancial.com>
2022-03-22 18:10:46 -0500

    Working with Index.html

--- src/cljs_chess/views.cljs
+++ src/cljs_chess/views.cljs
@@ -1,13 +1,29 @@
 (ns cljs-chess.views
-    (:require [clojure.string :refer [includes?]]
-              [re-frame.core :refer [subscribe dispatch]]
-              [taoensso.timbre :refer-macros [infof]]))
+  (:require [clojure.string :refer [includes?]]
+            [cljs-chess.components.drag-and-drop-board :as dnd-board]
+            [cljs-chess.components.chess-piece :as chess-piece]
+            [cljs-chess.chess :as chess :refer [STARTING-CHESS-BOARD]]
+            [cljs-chess.stories.helper :as helper]
+            [re-frame.core :refer [subscribe dispatch]]
+            [taoensso.timbre :refer-macros [infof]]
+            [reagent.core :as reagent]))
+
+(def state
+  (reagent/atom STARTING-CHESS-BOARD))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Landing pages
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defn home []
-  [:div])
+(defn home
+  []
+  [:div
+   [dnd-board/drag-and-drop-board {:rows      8
+                                   :cols      8
+                                   :tag       "game"
+                                   :state     @state
+                                   :item-type chess-piece/chess-piece
+                                   :can-drop? chess/can-drop?
+                                   :on-drop   (partial chess/on-drop-handler state)}]])
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Test pages
